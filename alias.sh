868c87e53bd917b68e45781ac8b4f8bd1ed33289
bob chase <bob.chase@gmail.com>
2023-08-15 21:37:59 -0400

    add run alias

--- alias.sh
+++ alias.sh
@@ -1,5 +1,6 @@
 alias black="$(make bin)/black"
 alias pylint="$(make bin)/pylint"
 alias pytest="$(make bin)/pytest"
+alias run="$(make bin)/python3"
 
-alias un-alias="unalias black pylint pytest"
+alias un-alias="unalias black pylint pytest run"
